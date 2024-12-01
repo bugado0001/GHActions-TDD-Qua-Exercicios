9612e85ba2ae9bf65175924d36895826301f227b
Rafael <rafael317889@gamil.com>
2024-12-01 20:33:30 -0300

    Implementação das funções para passar nos testes

--- src/math.js
+++ src/math.js
@@ -1,15 +1,70 @@
-function fatorial() {
+function fatorial(n) {
+  if (typeof n !== 'number' && typeof n !== 'string') {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) {
+    throw new Error("Não existe fatorial de número negativo");
+  }
+  if (n === 0) return 1;
 
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
-function fibonnacci() {
 
+function fibonnacci(n) { // Nome adaptado para fibonnacci com dois "n"
+  if (typeof n !== 'number' && typeof n !== 'string') {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 0) {
+    throw new Error("Não existe fibonnacci de número menor ou igual a zero");
+  }
+
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+
+  let a = 0, b = 1, temp;
+  for (let i = 3; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 }
-function ehPrimo() {
 
+function ehPrimo(n) {
+  if (typeof n !== 'number' && typeof n !== 'string') {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  n = Number(n);
+  if (isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) {
+    throw new Error("Não número primo de número menor ou igual a 1");
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
+// Exporta as funções
 module.exports = {
   fatorial,
-  fibonnacci,
+  fibonnacci, // Exportando com o nome esperado pelo teste
   ehPrimo
-}
+};
